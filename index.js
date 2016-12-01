18928bef76d9b3afc5f95852c26527c7c457ab62
Vladimir Kudinov <frux@yandex-team.ru>
2016-12-01 16:41:04 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 /* eslint no-return-assign: 0 */
-export default function baseComponent(target) {
+function defaultResolver(e) {
+	return e.target.value;
+}
+function defaultRef(name, c) {
+	this[name] = c;
+}
+function wrappedRef(name, c) {
+	this[name] = c.getWrappedInstance();
+}
+
+module.exports = function baseComponent(target) {
 	target._bindedRefs = {};
 	target._bindedHandlers = {};
 
-	target.prototype._bind = function (...args) {
-		args.forEach(handlerName => {
+	target.prototype._bind = function () {
+		var max = arguments.length;
+		var i;
+		for (i = 0; i < max; i++) {
+			var handlerName = arguments[i];
 			this[handlerName] = this[handlerName].bind(this);
-		});
+		}
 	};
 
 	target.prototype._bindToState = function (key, resolver) {
-		const cachedHandler = this._bindedHandlers[key];
+		var cachedHandler = this._bindedHandlers[key];
 		if (cachedHandler) {
 			return cachedHandler;
 		}
-		if (typeof resolver === 'function') {
-			return e => this.setState({[key]: resolver(e)});
-		}
-		return e => this.setState({[key]: e.target.value});
+		resolver = (resolver || defaultResolver).bind(this);
+		return (function (e) {
+			var newState = {};
+			newState[key] = resolver(e);
+			this.setState(newState);
+		}).bind(this);
 	};
 
 	target.prototype._refCallback = function (name, wrapped) {
-		const cachedRef = this._bindedRefs[name];
-		if (cachedRef) {
-			return cachedRef;
-		}
-		if (wrapped) {
-			this._bindedRefs[name] = c => this[name] = c && c.getWrappedInstance();
-		} else {
-			this._bindedRefs[name] = c => this[name] = c;
+		if (!this._bindedRefs[name]) {
+			if (wrapped) {
+				this._bindedRefs[name] = wrappedRef.bind(this, name);
+			} else {
+				this._bindedRefs[name] = defaultRef.bind(this, name);
+			}
 		}
 		return this._bindedRefs[name];
 	};
-}
+};
